c46717e95f55c1a8775729455456b27ef8c1906e
shayaf <saya2yn@gmail.com>
2022-01-30 17:35:19 +0900

    modify: file name

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Index from '@/views/Index.vue'
-import PaymentComponent from '@/components/paymentComponent'
+import PaymentComponent from '@/components/PaymentComponent'
 Vue.use(VueRouter)
 
 const routes = [
